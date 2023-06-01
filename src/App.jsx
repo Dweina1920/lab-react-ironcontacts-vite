93d9360580fde5b1e97046f7d9e82f33f6267ea9
Dina <dina.yamanid@gmail.com>
2023-06-01 14:29:01 +0200

    emmy and Oscar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
 import "./App.css";
+import contacts from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const [allContacts, setAllContacts] = useState([...contacts]);
+
+  // criterio de ordenación
+  // const [sort, setSort] = useState("");
+  // if (sort == "popularity") {
+  //   const sortedContacts = [...allContacts];
+  //   sortedContacts.sort();
+  //   setAllContacts();
+  // }
+
+  let listFive = allContacts.map((person) => (
+    <tr>
+      <th>
+        <img className="logo" src={person.pictureUrl} />
+      </th>
+      <th>{person.name}</th>
+      <th>{person.popularity.toFixed(2)}</th>
+      <th>{person.wonOscar ? "🏆" : ""}</th>
+      <th>{person.wonEmmy ? "🏆" : ""}</th>
+    </tr>
+  ));
   return (
     <div className="App">
       <h1>LAB | React IronContacts </h1>
+      <button>Sort by popularity</button>
+      <button>Sort by name</button>
+      <table>
+        <thead>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+        </thead>
+        <tbody>{listFive}</tbody>
+      </table>
     </div>
   );
 }
